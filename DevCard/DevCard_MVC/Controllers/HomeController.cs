98c387f346f06c7c1c8424d701fa81984ae64d34
Hessamsd <hessamsadeghi73@gmail.com>
2024-08-27 14:49:22 +0330

    index & contact page added

--- DevCard/DevCard_MVC/Controllers/HomeController.cs
+++ DevCard/DevCard_MVC/Controllers/HomeController.cs
@@ -4,29 +4,32 @@ using System.Diagnostics;
 
 namespace DevCard_MVC.Controllers
 {
-    public class HomeController : Controller
-    {
-        private readonly ILogger<HomeController> _logger;
+	public class HomeController : Controller
+	{
 
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
 
-        public IActionResult Index()
-        {
-            return View();
-        }
+		public HomeController()
+		{
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
+		}
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
-    }
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+
+		public IActionResult Contact()
+		{
+
+			return View();
+
+		}
+
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		public IActionResult Error()
+		{
+			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+		}
+	}
 }
